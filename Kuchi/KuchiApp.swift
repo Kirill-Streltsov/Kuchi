7bb5880ca3d443c12fe0e9d9cbce32fe498cee1a
Kirill-Streltsov <krlsrlsv@gmail.com>
2023-09-02 18:02:52 +0200

    Initial Commit

---
+++ Kuchi/KuchiApp.swift
@@ -0,0 +1,17 @@
+//
+//  KuchiApp.swift
+//  Kuchi
+//
+//  Created by Kirill Streltsov on 02.09.23.
+//
+
+import SwiftUI
+
+@main
+struct KuchiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
