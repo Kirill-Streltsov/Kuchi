36cb3ade0c5979be2d79d2ad4df5ac6643714887
Kirill-Streltsov <krlsrlsv@gmail.com>
2023-09-03 23:49:47 +0200

    finished RegisterView

--- Kuchi/Welcome/RegisterView.swift
+++ Kuchi/Welcome/RegisterView.swift
@@ -11,7 +11,7 @@ struct RegisterView: View {
     
     
     @FocusState var nameFieldFocused: Bool
-    @State var userManager = UserManager()
+    @EnvironmentObject var userManager: UserManager
     
     var body: some View {
         VStack {
@@ -20,7 +20,30 @@ struct RegisterView: View {
             TextField("Type your name...", text: $userManager.profile.name)
                 .focused($nameFieldFocused)
                 .submitLabel(.done)
+                .onSubmit(registerUser)
                 .bordered()
+            HStack {
+                Spacer()
+                Toggle(isOn: $userManager.settings.rememberUser) {
+                    Text("Remember me")
+                        .font(.subheadline)
+                        .foregroundColor(.gray)
+                }
+                .fixedSize()
+            }
+            Button(action: self.registerUser) {
+                HStack {
+                    Image(systemName: "checkmark")
+                        .resizable()
+                        .frame(width: 16, height: 16, alignment: .center)
+                    Text("OK")
+                        .font(.body)
+                        .bold()
+                    
+                }
+            }
+            .disabled(!userManager.isUserNameValid())
+            .bordered()
             Spacer()
         }
         .padding()
@@ -29,8 +52,29 @@ struct RegisterView: View {
     }
 }
 
+// MARK: - Event Handlers
+extension RegisterView {
+    
+    func registerUser() {
+        
+        nameFieldFocused = false
+        
+        if userManager.settings.rememberUser {
+            userManager.persistProfile()
+        } else {
+            userManager.clear()
+        }
+        
+        userManager.persistSettings()
+        userManager.setRegistered()
+    }
+}
+
 struct RegisterView_Previews: PreviewProvider {
+    static let user = UserManager(name: "Ray")
+    
     static var previews: some View {
         RegisterView()
+            .environmentObject(user)
     }
 }
